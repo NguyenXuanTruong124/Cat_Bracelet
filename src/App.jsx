8820f7830f7b74ed77eb45c7041cddfae85c8bef
Truong <xuantruong124.work@gmail.com>
2026-01-07 23:34:17 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,40 +17,28 @@ import StaffCustomers from './pages/Staff/StaffCustomers.jsx';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/role" element={<RoleSelection />} />
-
-      <Route path="/staff" element={<Navigate to="/staff/dashboard" replace />} />
-      <Route path="/staff/dashboard" element={<StaffDashboard />} />
-      <Route path="/staff/products" element={<StaffProducts />} />
-      <Route path="/staff/orders" element={<StaffOrders />} />
-      <Route path="/staff/customers" element={<StaffCustomers />} />
-
-      <Route path="/customer" element={<Customer />} />
-      <Route path="/admin" element={<Admin />} />
-
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
-=======
     <CartProvider>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/role" element={<RoleSelection />} />
-        <Route path="/staff" element={<Staff />} />
+
+        <Route path="/staff" element={<Navigate to="/staff/dashboard" replace />} />
+        <Route path="/staff/dashboard" element={<StaffDashboard />} />
+        <Route path="/staff/products" element={<StaffProducts />} />
+        <Route path="/staff/orders" element={<StaffOrders />} />
+        <Route path="/staff/customers" element={<StaffCustomers />} />
+
         <Route path="/customer" element={<Customer />} />
         <Route path="/about" element={<About />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/admin" element={<Admin />} />
+
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
       <CartDrawer />
     </CartProvider>
->>>>>>> UI_Customer
   );
 }
 
